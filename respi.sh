71f813a40940788348b5a5d810bab7797468e2ec
paco_pokemaster12@hotmail.com <paco_pokemaster12@hotmail.com>
2017-08-31 14:48:24 -0700

    base

--- respi.sh
+++ respi.sh
@@ -1,7 +1,7 @@
 sshpass -p "holamundo" scp /home/mia/repo/redes3/respaldo.sql eduardo@148.211.152.133:/home/eduardo
-mysql -u root -p prueba > /home/mia/respaldo2.sql
-mysql -u root -p "hola"
-drop database prueba;
-createdatabase prueba:
-use prueba;
-source /home/mia/respaldo.sql;
+#mysql -u root -p prueba > /home/mia/respaldo2.sql
+#mysql -u root -p "holamundo"
+#drop database prueba;
+#createdatabase prueba:
+#use prueba;
+#source /home/mia/respaldo.sql;
