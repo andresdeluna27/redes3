af59e9c0177f3d13a5e11de4aeb53ec286a5fb7f
paco_pokemaster12@hotmail.com <paco_pokemaster12@hotmail.com>
2017-11-15 14:57:32 -0800

    proced

--- backup.sh
+++ backup.sh
@@ -4,7 +4,10 @@ FECHA=$(date +%F+%T)
 USUARIO=$(whoami)
 DIRECTORIOS="/home/prueba"
 function Completo {
-    tar -cvzf /home/prueba/respaldo-$FECHA.tgz $DIRECTORIOS
+    #tar -cvzf /home/prueba/respaldo-$FECHA.tgz $DIRECTORIOS
+	tar -cpvzf "/home/prueba-inc/fullbackup_`date +%d%m%Y`.tgz" -g /home/prueba-inc/backup.snap /home/prueba/*
+	tar -cpvzf "/home/prueba2-dif/fullbackup_DIF_`date +%d%m%Y`.tgz" /home/prueba2/*
+	#tar -cpvzf "inc_backup_`date +%d%m%Y`.tgz" -g /home/alumno/Documentos/R_Inc/backup.snap /home/alumno/Documentos/Files/*
     
     #if [ $? -ne 0 ]; then
      #   logger "Error en el respaldo de $FECHA"
@@ -15,10 +18,21 @@ function Completo {
     #fi
 }
 
+function Incre {
+	if [ -e /home/prueba-inc/backup.snap ]; then
+		tar -cpvzf "/home/prueba-inc/inc_backup_`date +%d%m%Y`.tgz" -g /home/prueba-inc/backup.snap /home/prueba/*
+	else
+		Completo
+	fi
+}
+
 function Diferencial {
-    if [ -e /home/mia/repo/ult_resp ]; then
+    #if [ -e /home/mia/repo/ult_resp ]; then
+    if [ -e /home/prueba2-dif/fullbackup* ]; then
         ULT-RESP=$(cat /home/mia/repo/ult_resp)
-        tar -cvzN $FECHA -f /home/mia/repo/respaldo-$FECHA.tgz $DIRECTORIOS
+        #tar -cvzN $FECHA -f /home/mia/repo/respaldo-$FECHA.tgz $DIRECTORIOS
+	
+	tar -cpvzf "/home/prueba2-dif/dif1_backup_`date +%d%m%Y`.tar.gz" /home/prueba2/* -N 14-nov-17
             #if [ $? -ne 0 ]; then
                     #logger "Error en el respaldo de $FECHA"
                     #return 1                                  
@@ -34,7 +48,7 @@ function Diferencial {
 #main
 if [ $# -ne 1 ]; then
     echo "Error, numero de opciones incorrectas"
-    echo "Uso: $0 -c ó $0 -d"
+    echo "Uso: $0 -c ó $0 -d $0 -i"
     exit 1
 else
     if [ $USUARIO != root ]; then
@@ -44,6 +58,7 @@ else
     case $1 in
         -c)Completo;;
         -d)Diferencial;;
+	-i)Incre;;
         *) echo "Opcion incorrecta";exit 1;;
     esac
 fi 
